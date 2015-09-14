14840659c186b16ab3555a989957c881d9dedaca
Andre Bossard <abossard@gmail.com>
2015-09-14 17:33:25 +0200

    new version

---
+++ client/layouts/CoreLayout.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import 'styles/core.scss';
+
+export default class CoreLayout extends React.Component {
+  static propTypes = {
+    children : React.PropTypes.element
+  }
+
+  /** @namespace this.props */
+
+  constructor () {
+    super();
+  }
+
+  render () {
+    return (
+      <div className='page-container'>
+        <div className='view-container'>
+          {this.props.children}
+        </div>
+      </div>
+    );
+  }
+}
